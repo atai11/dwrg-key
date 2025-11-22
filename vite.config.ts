0992d58ea68888b8ce60e234b82a98cccecbee1c
atai11 <1183954070@qq.com>
2025-11-22 21:37:00 +0800

    2025/11/22

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {viteSingleFile} from "vite-plugin-singlefile";
 
 // https://vite.dev/config/
+// export default defineConfig({
+//     plugins: [vue()],
+// })
+
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [viteSingleFile(),vue()],
+    build: {
+        assetsInlineLimit: 4096 * 1024 * 1024
+    }
 })
